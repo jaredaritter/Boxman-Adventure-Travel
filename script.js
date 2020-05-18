787fc1f7df4b7d494609fd13dd76f23643b7bc10
jared <38332939+jaredaritter@users.noreply.github.com>
2020-05-18 16:58:34 -0400

    Added picture to left side of survey when choice is made. Currently printing additional image on click.

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 const buttons = document.querySelectorAll('button');
 const sign = document.querySelector('#sign');
+const surveyImg = document.querySelector('#surveyImg');
 
-buttons.forEach(button => button.addEventListener('click', showSign));
+buttons.forEach(button => button.addEventListener('click', show));
 
-
-function showSign(e) {
+function show(e) {
     const p = document.createElement('p');
     p.textContent = `You're going to love your ${e.target.value} adventure!`;
-    // while (sign.firstChild) {
-    //     sign.removeChild(sign.lastChild);
-    // }
     if (sign.children[1]){
         sign.removeChild(sign.lastChild);
     }
     sign.appendChild(p);
     sign.classList.remove('fadeout');
     sign.classList.add('fadein');
+
+    const img = document.createElement('img');
+    img.setAttribute('src', './images/feast-steak.jpg');
+    img.setAttribute('alt', 'steak');
+    img.classList.add('window');
+    surveyImg.appendChild(img);
+    surveyImg.classList.remove('fadeout');
+    surveyImg.classList.add('fadein');
 }
